9d5529a46345aa5db76110abc1fe51cecb04330a
Jim Rosser <jarosser06@gmail.com>
2014-12-26 14:40:17 -0600

    added memcached provider

--- redis/partials/iptables.rb
+++ redis/partials/iptables.rb
@@ -6,7 +6,7 @@ search_add_iptables_rules("chef_environment:#{node.chef_environment}",
                           9999,
                           'Open port for redis')
 |{ else if eq .Options.Openfor "all" }|
-search_add_iptables_rules("node:*",
+search_add_iptables_rules("nodes:*",
                           'INPUT',
                           "-m tcp -p tcp --dport #{node['redis-multi']['bind-port']} -j ACCEPT",
                           9999,
